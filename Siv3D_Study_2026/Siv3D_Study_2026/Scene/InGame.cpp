a6b10af9068d4053cb538fa5bf5f66ffd6663998
RyotaIzumi <136326820+RyotaIzumi@users.noreply.github.com>
2026-06-10 17:17:25 +0900

    シーンシステムの基礎、タイトルシーン、インゲームシーン作成

---
+++ Siv3D_Study_2026/Siv3D_Study_2026/Scene/InGame.cpp
@@ -0,0 +1,19 @@
+﻿#include "Scene.h"
+
+// インゲームシーンのコンストラクタ
+InGame::InGame(const InitData& data) : IScene(data) {
+}
+
+// インゲームシーンの更新処理
+void InGame::update() {
+	// Qキーが押されたら、タイトルシーンに遷移する
+	if (KeyQ.down()) {
+		changeScene(SceneType::Title, TRANSITION_DURATION);
+	}
+}
+
+// インゲームシーンの描画処理
+void InGame::draw() const {
+	// 薄緑の背景を描画
+	Scene::SetBackground(ColorF{ 0.6, 0.8, 0.7 });
+}
---
+++ Siv3D_Study_2026/Siv3D_Study_2026/Scene/Scene.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+// シーン変更時のフェードイン・アウトの時間
+static const Duration TRANSITION_DURATION = 0.50s;
+
+// シーンの種類
+enum class SceneType {
+	Title,
+	IN_GAME
+};
+
+using App = SceneManager<SceneType>;
+
+// ----- 各シーンのクラス ----- //
+
+class Title : public App::Scene {
+private:
+public:
+	Title(const InitData& data);
+
+	void update() override;
+	void draw() const override;
+};
+
+class InGame : public App::Scene {
+private:
+public:
+	InGame(const InitData& data);
+
+	void update() override;
+	void draw() const override;
+};
